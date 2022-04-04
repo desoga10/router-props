3ed2a4c0cb394d2ec0764a4b3e991e4c0889a1c9
desoga10 <thecodeangle@gmail.com>
2022-04-04 03:58:25 +0100

    Added validation

--- src/ComponentOne.js
+++ src/ComponentOne.js
@@ -2,15 +2,25 @@ import React, { useState } from 'react'
 import { Link } from 'react-router-dom';
 
 const ComponentOne = () => {
-  const [number, setNumber] = useState()
+  const [number, setNumber] = useState("")
 
-  console.log(number);
+  console.log(number.length);
   return (
     <>
       <div>ComponentOne</div>
       <input type="number" name="number" onChange={e => setNumber(e.target.value)} />
       <br />
-      <button>  <Link to={{ pathname: "/two" }} state={{ phoneNumber: number }}>send</Link></button>
+
+      <small style={{ color: 'red' }}>{number.length <= 0 && "Field cannot be empty"}</small>
+
+      <br />
+      <button style={number.length <= 0 ? { cursor: "not-allowed" } : null}>
+        <Link
+          style={number.length <= 0 ? { pointerEvents: "none" } : null}
+          to={{ pathname: "/two" }}
+          state={{ phoneNumber: number }}>send
+        </Link>
+      </button>
     </>
   )
 }
